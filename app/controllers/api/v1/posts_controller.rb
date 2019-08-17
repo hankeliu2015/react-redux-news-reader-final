ac14c5e5be2bf661829000bc207b92921360ac3f
github username <github email address>
2019-08-17 15:24:12 -0400

    add index action into post controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,20 +2,30 @@ module Api
   module V1
     class PostsController < ApplicationController
 
-    def create
-      if user_signed_in?
-        if post = current_user.posts.create(post_params)
-          render json: post, status: :created
+      def index
+        if user_signed_in?
+          render json: current_user.posts
         else
-          render json: post.errors, status: 400
+          render json: {}, status: 401
         end
-      else
-        render json: {}, status: 401
       end
-    end
+      
+      def create
+        if user_signed_in?
+          if post = current_user.posts.create(post_params)
+            render json: post, status: :created
+          else
+            render json: post.errors, status: 400
+          end
+        else
+          render json: {}, status: 401
+        end
+      end
 
-    def post_params
-      params.require(:post).permit(:title, :content)
+      def post_params
+        params.require(:post).permit(:title, :content)
+      end
+
     end
   end
 end
