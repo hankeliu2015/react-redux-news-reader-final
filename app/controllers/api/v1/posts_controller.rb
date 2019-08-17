ece6e3e93c0fe42616bb243280b4dfa1acdd7b1a
github username <github email address>
2019-08-17 14:32:35 -0400

    add create action in Post controller and strong pramas."

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,6 +1,21 @@
-module Api 
+module Api
   module V1
     class PostsController < ApplicationController
+
+    def create
+      if user_signed_in?
+        if post = current_user.posts.create(post_params)
+          render json: post, status: :created
+        else
+          render json: post.errors, status: 400
+        end
+      else
+        render json: {}, status: 401
+      end
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :content)
     end
   end
 end
