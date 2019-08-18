dccfe05065290cada4a55057970ff771026ff518
github username <github email address>
2019-08-17 21:58:48 -0400

    add combine reducer index.js and commentReducer

---
+++ client/src/reducers/commentReducer.js
@@ -0,0 +1,18 @@
+export default function commentReducer(state={comments: [], loading: false}, action) {
+  switch(action.type) {
+
+    case 'UPLOADING_COMMENT':
+      return {...state, loading:true};
+
+    case 'ADDING_COMMENT':
+      return {comments: [...state.comments, action.payload], loading: false}
+
+    case 'FETCHING_COMMENTS':
+      return {...state, loading: true};
+
+    case 'LOADING_COMMENTS':
+      return {comments: action.payload, loading: false}
+    default:
+      return state;
+  }
+}
