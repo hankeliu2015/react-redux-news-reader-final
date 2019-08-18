2cb366854bff450b7c29ce138cff7f06cd8449aa
github username <github email address>
2019-08-17 22:03:49 -0400

    add restore store, provider and redux dev tool extention in index.js'

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,21 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer,  composeEnhancer(applyMiddleware(thunk)),
+);
+
+
+ReactDOM.render(
+
+  <Provider store={store}>
+    <App /> 
+  </Provider>,
+
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
