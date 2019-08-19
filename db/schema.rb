744eabaa60ad1d07910bf9c4b6ad1a39545dad25
github username <github email address>
2019-08-19 12:06:39 -0400

    add Comment resource and run db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_014531) do
+ActiveRecord::Schema.define(version: 2019_08_19_160434) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "story_id"
+    t.string "story_comment"
+    t.integer "story_score"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
