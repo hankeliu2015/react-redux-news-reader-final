81531c80f369c9be152bf37f2694477296f26ffb
github username <github email address>
2019-08-19 14:06:46 -0400

    add strong params to Comment controller and updated create controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,25 @@
 class CommentsController < ApplicationController
-  def index 
+  def index
         if user_signed_in?
           render json: current_user.comments
         else
           render json: {}, status: 401
         end
       end
+
+  def create
+    if user_signed_in?
+      if comment = current_user.comments.create(comment_params)
+        render json: comment, status: :created
+      else
+        render json: comment.errors, status: 400
+      end
+    else 
+      render json: {}, status: 401
+    end
+  end
+
+  def comment_params
+      params.require(:comment).permit(:story_id, :story_comment, :story_score, :user_id)
+  end
 end
