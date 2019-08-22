9199549883e7326839359968d568e0951db52daa
github username <github email address>
2019-08-21 21:32:38 -0400

    import postReducer to combineReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers} from 'redux';
 import commentReducer from './commentReducer';
 import storyReducer from './storyReducer';
+import postReducer from './postReducer';
 
 export default combineReducers({
-  commentReducer, storyReducer
+  commentReducer, storyReducer, postReducer
 })
