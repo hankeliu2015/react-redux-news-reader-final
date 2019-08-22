bd52e09c60283d40a03e16b6b740dfb2578bdeaa
github username <github email address>
2019-08-22 16:56:36 -0400

    clean up syntax comments and change list map function to curl bracket style

--- client/src/containers/Stories.js
+++ client/src/containers/Stories.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchStories } from '../actions/storyAction';
 import { Link } from 'react-router-dom';
-import StoriesList from '../components/storiesList'
+import StoriesList from '../components/storiesList';
 
 class Stories extends Component {
 
@@ -11,20 +11,14 @@ class Stories extends Component {
   }
 
   render() {
+    const list = this.props.stories.map((story, index) => {
 
-    const list = this.props.stories.map((story, index) => <li key={index}><Link to={`${story ? "/comments/" + story.id : 'stories'}`}> {story ? story.title : "This Story is missing"}</Link></li>)
-
-    // const list = this.props.stories.map((story, index) => <li key={index}><Link to={`/storycomments/${story.id}`}> {story.title}</Link></li>)
-
-    // const list = () => {
-    //   if (this.props.stories.length ===0) {
-    //     return (
-    //       this.props.stories.map((story, index) => <li key={index}><a href="/story" data-storyid={story.id}> {story.title}</a></li>)
-    //     )
-    //     } else {
-    //       return "...loading"
-    //   }
-    // }
+      return (
+        <li key={index}>
+          <Link to={`${story ? "/comments/" + story.id : 'stories'}`}> {story ? story.title : "This Story is missing"}</Link>
+        </li>
+      )
+    })
 
     return (
       <div>
