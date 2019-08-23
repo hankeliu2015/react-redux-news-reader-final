0129c73ee19e91ad2d6ee9f3f28e48ee3dabf16d
github username <github email address>
2019-08-22 21:18:15 -0400

    wip - need a ternary conditino in jsx to show user published post after click on submit

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -5,8 +5,9 @@ export default function postList(props) {
     return (
       <div>
 
-      <h4>Your story is successfully published:</h4>
-      {props.userPosts}
+      <h6>
+        {props.userPosts.length !== 0 ? ("Your Post is successfully Published" + props.userPosts) : "Please click on Submit to Publish your Post"}
+      </h6>
 
       <h5>Please check other authors published story:</h5>
       {props.loading ? "...Posts Loading" : props.posts}
