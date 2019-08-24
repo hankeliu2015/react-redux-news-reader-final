fca700408e3e62b0c76b7d70cd15edcfccb8ed91
github username <github email address>
2019-08-24 15:28:22 -0400

    add connect mapstatetoprops and add confimation message for user submit post.

--- client/src/components/newPost.js
+++ client/src/components/newPost.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux'
 import { postUpload } from '../actions/postCreateAction';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form'
-
 
 class NewPost extends Component {
   state = {
@@ -21,7 +18,11 @@ class NewPost extends Component {
     event.preventDefault();
     let valueCSRF = document.querySelector('meta[name="csrf-token"]').content;
     this.props.postUpload(this.state, valueCSRF);
-
+    let postContent = this.state.Content
+    this.setState({
+      content: '',
+      title:''
+    })
   }
 
   render() {
@@ -39,9 +40,19 @@ class NewPost extends Component {
           <button variant="outline-primary" type="submit">Submit</button>
         </form>
 
+        <h6>
+          {this.props.userPosts.length !== 0 ? ("Your Post is successfully Published") : "Please click on Submit to Publish your Post"}
+        </h6>
+
       </div>
     )
   }
 }
 
-export default connect(null, {postUpload})(NewPost)
+const mapStateToProps = state => {
+  return {
+    userPosts: state.postReducer.userPosts
+  }
+}
+
+export default connect(mapStateToProps, {postUpload})(NewPost)
