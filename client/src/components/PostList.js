80750cfc3e36484c26fb2f026233d79c94c0935a
github username <github email address>
2019-08-24 15:30:15 -0400

    clear up comment and syntax format

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 export default function postList(props) {
-
     return (
       <div>
+        <h5>
+            Welcome to our users posts and please add your own comments:
+        </h5>
+        {props.loading ? "...Posts Loading" : props.posts}
 
-      <h6>
-        {props.userPosts.length !== 0 ? ("Your Post is successfully Published" + props.userPosts) : "Please click on Submit to Publish your Post"}
-      </h6>
-
-      <h5>Please check other authors published story:</h5>
-      {props.loading ? "...Posts Loading" : props.posts}
-
+        {/*<h6>
+            {props.userPosts.length !== 0 ? ("Your Post is successfully Published" + props.userPosts) : "Please click on Submit to Publish your Post"}
+          </h6> */}
       </div>
     )
 }
