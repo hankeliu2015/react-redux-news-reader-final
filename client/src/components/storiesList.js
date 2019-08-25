4cb791979310f38878b9094dcb810faad5365f37
github username <github email address>
2019-08-24 20:38:12 -0400

    remove the horrizontal line under react logo

--- client/src/components/storiesList.js
+++ client/src/components/storiesList.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function StoriesList(props) {
   return (
     <div>
-      <h4 style={{ borderTop: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}> </h4>
+      {/*
+        <h4 style={{ borderTop: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}> </h4>
+      */}
       <ol>
         {props.loading ? "...Stories Loading" : props.list}
       </ol>
