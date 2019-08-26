01bc4cceb7a31988390339fa867e015221944321
github username <github email address>
2019-08-26 17:18:22 -0400

    create currentUserAction creator

---
+++ client/src/actions/currentUserAction.js
@@ -0,0 +1,13 @@
+export default function currentUserFetch() {
+  return dispatch => {
+    dispatch({type: 'LOADING_USER'});
+    return (
+      fetch('http://localhost:3000/api/v1/current_user')
+      .then(resq => resq.json())
+      .then(user => {
+        dispatch({type: 'FETCHING_USER', payload: user })
+      })
+      .catch(function(error) {console.log('There has been a problem with your posts fetch operation:', error.message)})
+    )
+  }
+}
