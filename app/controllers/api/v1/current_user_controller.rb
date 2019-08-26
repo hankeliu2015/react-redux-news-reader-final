b2c8c02da7007ebdcec522051ab29aea9ec854d9
github username <github email address>
2019-08-26 16:37:19 -0400

    add current_user_controller to create a API for username

---
+++ app/controllers/api/v1/current_user_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class CurrentUserController < ApplicationController
+
+      def index
+        if user_signed_in?
+          render json: current_user.name
+        else
+          render json: {}, status: 401
+        end
+      end
+
+    end
+  end
+end
