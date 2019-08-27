2e4f3de3dca0af2097ed1644661cba065818fac2
github username <github email address>
2019-08-27 16:17:55 -0400

    adjusting fetch api url

--- client/src/actions/singlePostAction.js
+++ client/src/actions/singlePostAction.js
@@ -2,7 +2,7 @@ export function fetchSinglePost(postId) {
   return (dispatch) => {
     dispatch({type: 'LOADING_SINGLE_POST'});
     return (
-      fetch(`http://localhost/api/v1/posts/:id`)
+      fetch(`http://localhost:3000/api/v1/posts/${postId}`)
       .then(resp => resp.json())
       .then(post => {
         dispatch({type: 'FETCH_SINGLE_POST', payload: post})
