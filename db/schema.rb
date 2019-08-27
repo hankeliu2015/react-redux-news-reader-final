96b57676b1ed1424ab8d15851235484680cf6fe8
github username <github email address>
2019-08-27 12:41:31 -0400

    add post as foreign key to comments. Set up has_mang and belongs_to relationship. set the post as optional foreign key for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_173109) do
+ActiveRecord::Schema.define(version: 2019_08_27_163513) do
 
   create_table "comments", force: :cascade do |t|
     t.string "story_id"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_08_23_173109) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
