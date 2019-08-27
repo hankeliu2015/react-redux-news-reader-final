1cd6d085dc0d32fb8ad88adbac5420f48f517e51
github username <github email address>
2019-08-27 14:12:08 -0400

    change header.js from class component to stateless functional component.

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from '../logo.svg';
 
-class Header extends Component {
-  render(){
-    return (
-      <div>
+export default function header(props) {
+  return (
+    <div>
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-      {this.props.username ? `Hi ${this.props.username}!` : '' } Welcome to News Feed from Hacker News. Please sign in to add your comments or post your own stories.
+      {props.username ? `Hi ${props.username}!` : '' } Welcome to News Feed from Hacker News. Please sign in to add your comments or post your own stories.
       </p>
       </header>
       </div>
-    )
-  }
+  )
 }
-
-export default Header
