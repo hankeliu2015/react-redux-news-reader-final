cca8c2c65b352e94a63b34d37d18ae1faf72af0a
github username <github email address>
2019-08-27 15:21:03 -0400

    adjust posts card format and add button link for user add comments for each post

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -3,9 +3,6 @@ import React from 'react';
 export default function postList(props) {
     return (
       <div>
-        <h5>
-            Welcome to our users posts and please add your own comments:
-        </h5>
         {props.loading ? "...Posts Loading" : props.posts}
 
         {/*<h6>
--- client/src/containers/Posts.js
+++ client/src/containers/Posts.js
@@ -3,7 +3,9 @@ import NewPost from '../components/NewPost';
 import PostList from '../components/PostList';
 import { connect } from 'react-redux';
 import fetchPosts from '../actions/postsFetchAction';
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 class Posts extends Component {
 
@@ -20,10 +22,17 @@ class Posts extends Component {
       let dateString = postDate.toDateString()
 
       return(
-        <Card body key={post.id}>
-          <Card.Header>{post.title}</Card.Header>
+        <Card body key={post.id} border="light">
+          <Card.Header>
+            Created at: {dateString}; By: {post.user.username};
+          </Card.Header>
+          <Card.Title>{post.title}</Card.Title>
           <Card.Text>{post.content}</Card.Text>
-          <Card.Link href="#">Created at: {dateString}; By: {post.user.username}</Card.Link>
+          <Button variant="light">
+            <Link to={`${post ? "/postcomments/" + post.id : 'posts'}`}>
+              {post ? "Add Comments" : "This Post is missing"}
+            </Link>
+          </Button>
         </Card>
       )
     })
