4520ead5e67619f3fb2afdc12d433f597b9f6d5a
github username <github email address>
2019-08-27 23:06:42 -0400

    add post_id and belongs_to post relationship in comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :story_id, :story_comment, :created_at, :story_score
+  attributes :id, :story_id, :story_comment, :created_at, :story_score, :post_id
   belongs_to :user
+  belongs_to :post
 end
