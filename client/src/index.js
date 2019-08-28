793af98781cd2a9e325f0912a9f213bc0deab939
github username <github email address>
2019-08-28 16:00:03 -0400

    clear up comments and debuggers

--- client/src/index.js
+++ client/src/index.js
@@ -11,16 +11,12 @@ import rootReducer from './reducers'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer,  composeEnhancer(applyMiddleware(thunk)),
-);
-
+const store = createStore(rootReducer,  composeEnhancer(applyMiddleware(thunk)),);
 
 ReactDOM.render(
-
   <Provider store={store}>
     <App />
   </Provider>,
-
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
