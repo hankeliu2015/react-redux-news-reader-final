9b0f82bb2b969328dec6d78ac8ca5bfc8151f7e1
github username <github email address>
2019-08-27 23:08:54 -0400

    format change, no syntax change

--- client/src/components/commentList.js
+++ client/src/components/commentList.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function commentList(props)  {
     return (
       <div>
-        <h4>Comments - current user and other users's comment(s) fetched from Database</h4>
+        <h4>Comments - Current User and other users</h4>
         <ul>
           {props.displayComments}
         </ul>
