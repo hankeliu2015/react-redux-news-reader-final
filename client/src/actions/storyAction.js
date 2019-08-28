44958bf6ce9410a7a151a484840e40e562b9e6d2
github username <github email address>
2019-08-28 15:53:16 -0400

    clear up comments and debuggers'

--- client/src/actions/storyAction.js
+++ client/src/actions/storyAction.js
@@ -1,23 +1,20 @@
 export function fetchStories() {
-  // debugger
   return (dispatch) => {
     dispatch({type: 'LOADING_STORIES'});
     return (
       fetch('https://hacker-news.firebaseio.com/v0/askstories.json?print=pretty')
       .then(response => response.json())
       .then(storyIds => {
-        // debugger
         storyIds.map(function(id) {
           return (
             fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
             .then(response => response.json())
             .then(story => {
-               //console.log(story);
               dispatch({type: 'FETCHING_STORIES', payload: story})
               })
-          )     //1st return
-        })      //storyIds.map
-      })        //storyIds =>
-    )           //2nd return from above
+          )
+        })
+      })
+    )
   }
 }
