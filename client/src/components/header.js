ead5c7b76f52d58d1218e7219c81f827f21b57d5
github username <github email address>
2019-08-31 14:37:12 -0400

    format update. No syntax change

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -5,11 +5,13 @@ export default function header(props) {
   return (
     <div>
       <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-      {props.username ? `Hi ${props.username}!` : '' } Welcome to News Feed from Hacker News. Please sign in to add your comments or post your own stories.
-      </p>
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+        {props.username ? `Hi ${props.username}!` : '' } Welcome to News Feed from Hacker News. Please sign in to add your comments or post your own stories.
+        </p>
+      
       </header>
-      </div>
+    </div>
   )
 }
