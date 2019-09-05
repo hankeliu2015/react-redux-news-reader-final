1cd7c484a3993c008b3ae8f15a2f743d59731b3e
github username <github email address>
2019-09-04 22:23:11 -0400

    import and add connect component for storyCard component to access vote state and dispatch from redux store

--- client/src/components/StoryCard.js
+++ client/src/components/StoryCard.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
-import { likeVote } from '../actions/likeVoteAction'
+import { likeVote } from '../actions/likeVoteAction';
+import { connect } from 'react-redux';
 
 class StoryCard extends Component{
   state = {
@@ -19,7 +20,6 @@ class StoryCard extends Component{
   render () {
     return (
       <div>
-
         <Card body>
             <Link to={`${this.props.story ? "/comments/" + this.props.story.id : 'stories'}`}> {this.props.story ? this.props.story.title : "This Story is missing"}</Link>
 
@@ -30,18 +30,20 @@ class StoryCard extends Component{
             <Button variant="light">
               <form onSubmit = {this.handleOnClick}>
                 <button type="submit">Vote</button>
-
               </form>
             </Button>
 
         </Card>
-
-
-
       </div>
     )
   }
-
 }
 
-export default StoryCard
+
+const mapStateToProps = state => {
+  return {
+    vote: state.likeVoteReducer.vote
+  }
+}
+
+export default connect(mapStateToProps, {likeVote})(StoryCard)
