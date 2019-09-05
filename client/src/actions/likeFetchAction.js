7730ad79437ea681aeee262b48bec4a576b0af20
github username <github email address>
2019-09-05 15:49:28 -0400

    change fetchLike function to fetch have like object into payload, instead of only vote attribute

--- client/src/actions/likeFetchAction.js
+++ client/src/actions/likeFetchAction.js
@@ -6,8 +6,8 @@ export function fetchLike(itemId) {
       fetch(`http://localhost:3000/likes/${itemId}`)
       .then(resp => resp.json())
       .then(like => {
-        debugger
-        dispatch({type:'FETCHING_VOTE', payload: like.vote})
+        // debugger
+        dispatch({type:'FETCHING_VOTE', payload: like})
       })
       .catch(function(error) {console.log('There has been a problem with your fetch GET operation: ', error.message);})
 
