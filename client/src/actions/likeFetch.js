2a5da8615c0938796181efc9c61b233cc781cf84
github username <github email address>
2019-09-05 12:59:34 -0400

    add likeFetch action creator to retrieve vote from single story

---
+++ client/src/actions/likeFetch.js
@@ -0,0 +1,15 @@
+export function fetchLike(itemId) {
+
+  return dispatch => {
+    dispatch({type: 'LOADING_VOTE'});
+    return (
+      fetch('http://localhost:3000/likes/${itemId}')
+      .then(resp => resp.json())
+      .then(like => {
+        dispatch({type:'FETCHINGING_VOTE', payload: like.vote})
+      })
+      .catch(function(error) {console.log('There has been a problem with your fetch GET operation: ', error.message);})
+
+    )
+  }
+}
