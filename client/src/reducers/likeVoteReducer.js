dbb650811db14db3a8361ba751ea6539ad1ab24f
github username <github email address>
2019-09-04 22:28:14 -0400

    change vote increment by 1 for adding vote condition

--- client/src/reducers/likeVoteReducer.js
+++ client/src/reducers/likeVoteReducer.js
@@ -1,11 +1,11 @@
-export default function likeVoteReducer(state={vote: [], loading: false}, action) {
+export default function likeVoteReducer(state={vote: 0, loading: false}, action) {
   switch(action.type) {
 
     case 'UPLOADING_VOTE':
       return {...state, loading:true};
 
     case 'ADDING_VOTE':
-      return {vote: [...state.vote, action.payload], loading: false}
+      return {vote: state.vote + 1, loading: false}
 
     default:
       return state;
