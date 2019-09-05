c53354bc342cb97a703d993b7cccc6f49440974e
github username <github email address>
2019-09-04 22:04:51 -0400

    add like controller index and create action and strong params.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,25 @@
 class LikesController < ApplicationController
+  def index
+        if user_signed_in?
+          render json: Like.all
+        else
+          render json: {}, status: 401
+        end
+      end
+
+  def create
+    if user_signed_in?
+      if like = Like.create(like_params)
+        render json: like, status: :created
+      else
+        render json: like.errors, status: 400
+      end
+    else
+      render json: {}, status: 401
+    end
+  end
+
+  def like_params
+      params.require(:like).permit(:item_id, :vote)
+  end
 end
