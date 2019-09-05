3825f7b06b2ad5418f8689d3ca8e4fd9028499ba
github username <github email address>
2019-09-04 22:06:51 -0400

    remove syntax in stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,25 +1,3 @@
 class StoriesController < ApplicationController
-  def index
-        if user_signed_in?
-          render json: Story.all
-        else
-          render json: {}, status: 401
-        end
-      end
-
-  def create
-    if user_signed_in?
-      if story = Story.create(story_params)
-        render json: story, status: :created
-      else
-        render json: story.errors, status: 400
-      end
-    else
-      render json: {}, status: 401
-    end
-  end
-
-  def story_params
-      params.require(:story).permit(:story_id, :likes)
-  end
+  
 end
