1b0a2b6b7ed3188db75e9f223a08fce914643458
github username <github email address>
2019-09-06 10:59:12 -0400

    add like attribute to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_015743) do
+ActiveRecord::Schema.define(version: 2019_09_06_145716) do
 
   create_table "comments", force: :cascade do |t|
     t.string "story_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_09_05_015743) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "like"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
