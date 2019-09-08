7a500348f7ba73cc86babd5fabf48c971704d29b
github username <github email address>
2019-09-07 21:17:58 -0400

    disable likeFetch action creator and remove conditions from reducer

--- client/src/reducers/likeVoteReducer.js
+++ client/src/reducers/likeVoteReducer.js
@@ -1,4 +1,4 @@
-export default function likeVoteReducer(state={vote: 0, likes: [], allLikes: [], loading: false}, action) {
+export default function likeVoteReducer(state={vote: 0, allLikes: [], loading: false}, action) {
   switch(action.type) {
 
     case 'UPLOADING_VOTE':
@@ -7,16 +7,10 @@ export default function likeVoteReducer(state={vote: 0, likes: [], allLikes: [],
     case 'ADDING_VOTE':
       return {...state, vote: state.vote + 1, loading: false}
 
-    case 'LOADING_VOTE':
-      return {...state, loading:true};
+    case 'FETCHING_AllLIKES':
+    return {...state, allLikes: action.payload, loading: false}
 
-    case 'FETCHING_VOTE':
-      return {...state, likes: [...state.likes, action.payload], loading: false};
-
-      // return {...state, likes: {...state.likes, action.payload.item_id: action.payload}, loading: false}
-
-      case 'FETCHING_AllLIKES':
-      return {...state, allLikes: action.payload, loading: false}
+    // return {...state, allLikes: {...state.allLikes, action.payload.item_id: action.payload}, loading: false}
 
     default:
       return state;
