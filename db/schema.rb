aa3a8de6781b9df4c668817fb6dd47b0865f5fe8
Hank Liu <hanke.liu@gmail.com>
2020-05-12 15:43:23 -0400

    replaced sqlite with pg. created database and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,17 @@
 
 ActiveRecord::Schema.define(version: 2019_09_08_161627) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "story_id"
     t.string "story_comment"
     t.integer "story_score"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "post_id"
+    t.bigint "post_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -34,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_09_08_161627) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "like", default: 0
@@ -61,4 +64,7 @@ ActiveRecord::Schema.define(version: 2019_09_08_161627) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
