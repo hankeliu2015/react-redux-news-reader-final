3f87a5c9fafaae6bd983f67253c98aedc4b329fe
Hank Liu <hanke.liu@gmail.com>
2020-06-24 10:18:34 -0400

    add render conditions for logon and none logon users. fix likes post and get request errors."

--- client/src/containers/Stories.js
+++ client/src/containers/Stories.js
@@ -19,18 +19,30 @@ class Stories extends Component {
   }
 
   render() {
+    const currentUserName= this.props.user.username;
+
     const list = this.props.stories.map((story, index) => {
-    let foundLike = this.props.allLikes.find(like => parseInt(like.item_id) === (story ? story.id : -1))
-      return (
-        <li key={index}>
-          <StoryCard story = {story} like = {foundLike ? foundLike.vote : 0 } />
-        </li>
-      )
+      if(currentUserName){
+        let foundLike = this.props.allLikes.find(like => parseInt(like.item_id) === (story ? story.id : -1))
+
+        return (
+          <li key={index}>
+            <StoryCard story = {story} like = {foundLike ? foundLike.vote : 0 } />
+          </li>
+        )
+      } else {
+        let foundLike = 0;
+        return (
+          <li key={index}>
+            <StoryCard story = {story} like = {foundLike} />
+          </li>
+        )
+      }
     })
 
     return (
       <div>
-        <Header username = {this.props.user.username}/>
+        <Header username = {currentUserName}/>
         <StoriesList list={list} loading={this.props.loading} />
       </div>
     )
